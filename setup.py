02f12b87dc8d0f0571493d4951ae6a6a3a3f120a
aleks <aleks@hi-tt.com>
2014-04-06 10:11:16 +0400

    change parler version to >0.9.4

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     ],
     include_package_data=True,
     install_requires=[
-        'django-parler>=1.0',
+        'django-parler>0.9.4',
         'django-cms>=3.0c1',
         'django-taggit',
         'django-filer',
